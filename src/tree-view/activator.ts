0179751892f5cfb06a40d10cb9e38c799f2d4063
Prabushi Samarakoon <prabushisamarakoon@gmail.com>
2021-03-23 00:45:10 +0530

    Enable diagram upon tree item onClick

--- src/tree-view/activator.ts
+++ src/tree-view/activator.ts
@@ -16,9 +16,10 @@
  * under the License.
  */
 import { BallerinaExtension, ConstructIdentifier } from "../core";
+import { showDiagramEditor } from '../diagram';
 import { sendTelemetryEvent, CMP_PACKAGE_VIEW, TM_EVENT_OPEN_PACKAGE_OVERVIEW } from "../telemetry";
-import { commands, Range, TextDocumentShowOptions, Uri, ViewColumn, window } from 'vscode';
-import { PROJECT_KIND, TREE_ELEMENT_EXECUTE_COMMAND, TREE_REFRESH_COMMAND } from "./model";
+import { commands, window } from 'vscode';
+import { TREE_ELEMENT_EXECUTE_COMMAND, TREE_REFRESH_COMMAND } from "./model";
 import { PackageOverviewDataProvider } from "./tree-data-provider";
 
 export function activate(ballerinaExtInstance: BallerinaExtension) {
@@ -52,22 +53,24 @@ export function activate(ballerinaExtInstance: BallerinaExtension) {
         });
     });
 
-    ballerinaExtInstance.onPackageTreeElementClicked((construct) => {
-        openBallerinaFile(construct);
+    ballerinaExtInstance.onPackageTreeElementClicked((construct: ConstructIdentifier) => {
+        // openBallerinaFile(construct);
+        showDiagramEditor(ballerinaExtInstance.context!, ballerinaExtInstance.langClient!, construct.startLine,
+            construct.startColumn, construct.endLine, construct.endColumn, construct.filePath);
     });
 }
 
-function openBallerinaFile(construct: ConstructIdentifier) {
-    if (construct.filePath && (construct.kind === PROJECT_KIND.FUNCTION || construct.kind === PROJECT_KIND.RESOURCE)) {
-        const showOptions: TextDocumentShowOptions = {
-            preserveFocus: false,
-            preview: false,
-            viewColumn: ViewColumn.Active,
-            selection: new Range(construct.startLine!, construct.startColumn!, construct.startLine!, construct.startColumn!)
-        };
-        const status = commands.executeCommand('vscode.open', Uri.file(construct.filePath), showOptions);
-        if (!status) {
-            throw new Error(`Unable to open ${construct.filePath}`);
-        }
-    }
-}
+// function openBallerinaFile(construct: ConstructIdentifier) {
+//     if (construct.filePath && (construct.kind === PROJECT_KIND.FUNCTION || construct.kind === PROJECT_KIND.RESOURCE)) {
+//         const showOptions: TextDocumentShowOptions = {
+//             preserveFocus: false,
+//             preview: false,
+//             viewColumn: ViewColumn.Active,
+//             selection: new Range(construct.startLine!, construct.startColumn!, construct.startLine!, construct.startColumn!)
+//         };
+//         const status = commands.executeCommand('vscode.open', Uri.file(construct.filePath), showOptions);
+//         if (!status) {
+//             throw new Error(`Unable to open ${construct.filePath}`);
+//         }
+//     }
+// }
